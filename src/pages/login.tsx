cd77e7b86601a602bdc5110dc97052a8334d0413
Micicat123 <mici.zep@gmail.com>
2022-12-31 14:28:46 +0100

    finished login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,25 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component, SyntheticEvent, useEffect, useState } from 'react';
 import Wrapper from '../components/Wrapper';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
 const Login = () => {
 
-    const [loggedIn, setLoggedin] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    await axios.post('/auth/login', {email, password});
+
+    setRedirect(true);
+  };
+
+  if(redirect){
+    return <Navigate to={'/'}/>
+  }
 
     return (
       <Wrapper>  
@@ -14,14 +28,29 @@ const Login = () => {
                 <h4 className='login-heading'>Welcome <span className='orange-text'>back!</span></h4>
                 <p className='login-paragraph'>Thank you for coming back. Hope you have a good day and inspire others.</p>
             </div>
-            <form action="" className='login-form'>
+            <form onSubmit={submit} className='login-form'>
                 <label htmlFor="email"><p className='label-text'>Email</p></label>
-                <input type="email" id="email" /><br />
+                <input 
+                  type="email" 
+                  id="email" 
+                  placeholder="Email"
+                  required
+                  onChange={e => setEmail(e.target.value)}
+                />
+                <br />
 
                 <label htmlFor="password"><p className='label-text'>Password</p></label>
-                <input type="password" id="password"/><br />
+                <input 
+                  type="password" 
+                  id="password"
+                  placeholder="Password"
+                  required
+                  onChange={e => setPassword(e.target.value)}
+                />
+                <br />
 
                 <input type="submit" value="Login" className='submit-button'/>
+
                 <p>Dont have account?<span className='right-text'><a href="/signup" className='alternative-text'>Sign up</a></span></p>
             </form>
             <div>

0d4991f3edcd61ec76b83659519880c22c77066f
Micicat123 <mici.zep@gmail.com>
2022-12-31 23:42:03 +0100

    working on landing page

--- src/pages/landing-page.tsx
+++ src/pages/landing-page.tsx
@@ -5,15 +5,14 @@ import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
 
+    const [page, setPage] = useState(1);
     const [loggedIn, setLoggedin] = useState(false);
-
 
     useEffect(() => {
         (
             async () =>{
                 try{
-                    const {data} = await axios.get('/user'); //get authenticated user by jwt token 
-                    setLoggedin(true);
+                    const {data} = await axios.get(`quote/${page}`);
                 }catch(err){
                     console.log("User is not logged in.");
                 }
@@ -24,8 +23,20 @@ const LandingPage = () => {
     //user is logged in  
     if (loggedIn){
         return(
-            <>
-            </>
+            <Wrapper>  
+                <div className='index-div'>
+                    <div className='first-flex-item'>
+                        <h1 className='welcome-heading'>Welcome<br/> to <span className='orange-text'>Quotastic</span></h1>
+                        <h5 className='welcome-paragraph'>Quotastic is free online platform for you to explore the  quips, quotes, and proverbs. Sign up and express yourself.</h5>
+                        <Link to={'/signup'}>
+                            <button className="button signup-button"> <p  className='button-text'>Sign up</p></button>
+                        </Link>  
+                    </div> 
+                    <div className='second-flex-item'>
+                        <img src="/pictures/quotes-home.png" alt="Image"/>
+                    </div>             
+                </div>
+            </Wrapper>
         )
     }
 
@@ -41,7 +52,7 @@ const LandingPage = () => {
                 </Link>  
             </div> 
             <div className='second-flex-item'>
-                <img src="/pictures/quotes-home.png" alt="Image" className="" />
+                <img src="/pictures/quotes-home.png" alt="Image"/>
             </div>             
         </div>
       </Wrapper>
